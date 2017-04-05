1ebcf910776afc896f280fa00fe1852795b21a1e
Michelle Luo <luo.mich16@gmail.com>
2017-04-05 14:28:27 -0400

    Replace hardcoded strings

--- app/src/main/java/com/example/ian/twixter/PostActivity.java
+++ app/src/main/java/com/example/ian/twixter/PostActivity.java
@@ -8,14 +8,22 @@ import android.widget.Button;
 
 public class PostActivity extends AppCompatActivity {
 
-    Button cancelButton;
+    Button sendButton, cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        sendButton = (Button) findViewById(R.id.sendPost);
         cancelButton = (Button) findViewById(R.id.cancelPost);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(PostActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
