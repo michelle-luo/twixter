508e295e0c39a4017e21f8dc92a92090e63ea17d
Michelle Luo <luo.mich16@gmail.com>
2017-04-09 23:33:41 -0400

    Return to previous activity instead of creating new main activity

--- app/src/main/java/com/example/ian/twixter/MainActivity.java
+++ app/src/main/java/com/example/ian/twixter/MainActivity.java
@@ -1,44 +1,62 @@
 package com.example.ian.twixter;
 
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView header;
+    String headerText;
     Button feedButton, postButton, searchButton, dmButton, helpButton;
+    SendText sendSMS = new SendText();
+
+    protected void updateHeader() {
+        headerText = "You have sent " + Integer.toString(sendSMS.getSMSCount()) + " texts today.";
+        header.setText(headerText);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        header = (TextView) findViewById(R.id.msgToday);
         feedButton = (Button) findViewById(R.id.feedButton);
         postButton = (Button) findViewById(R.id.postButton);
         searchButton = (Button) findViewById(R.id.searchButton);
         dmButton = (Button) findViewById(R.id.dmButton);
         helpButton = (Button) findViewById(R.id.helpButton);
 
+        updateHeader();
+
         postButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                intent.putExtra("sendSMS", (Parcelable)sendSMS);
                 startActivity(intent);
+                updateHeader();
             }
         });
 
         feedButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                intent.putExtra("sendSMS", (Parcelable)sendSMS);
                 startActivity(intent);
+                updateHeader();
             }
         });
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                intent.putExtra("sendSMS", (Parcelable)sendSMS);
                 startActivity(intent);
+                updateHeader();
             }
         });
 
@@ -49,4 +67,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
--- app/src/main/java/com/example/ian/twixter/PostActivity.java
+++ app/src/main/java/com/example/ian/twixter/PostActivity.java
@@ -1,17 +1,17 @@
 package com.example.ian.twixter;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.SmsManager;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class PostActivity extends AppCompatActivity {
-
     Button sendButton, cancelButton;
+    SendText sendSMS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,37 +21,40 @@ public class PostActivity extends AppCompatActivity {
         sendButton = (Button) findViewById(R.id.sendPost);
         cancelButton = (Button) findViewById(R.id.cancelPost);
 
+        sendSMS = getIntent().getExtras().getParcelable("sendSMS");
+
         sendButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText editPost = (EditText) findViewById(R.id.editPost);
                 String message = editPost.getText().toString();
 
+                /* check message length */
                 if (message.length() == 0) {
                     Toast.makeText(getBaseContext(), "Please enter a Tweet.",
                             Toast.LENGTH_LONG).show();
-                    return;
                 }
                 if (message.length() > 160) {
-
                     Toast.makeText(getBaseContext(), "You are " +
                                     Integer.toString(message.length() - 160) +
                                     " characters over the 160 character limit, please try again!",
                             Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if (message.length() > 0 && message.length() <= 160) {
-                    SendText.sendText(getBaseContext(), "40404", message);
                 }
 
-                Intent intent = new Intent(PostActivity.this, MainActivity.class);
-                startActivity(intent);
+                /* send message */
+                sendSMS = sendSMS.sendText(getBaseContext(), "40404", message);
+
+                /* hide keyboard after click */
+                InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+
             }
         });
 
         cancelButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(PostActivity.this, MainActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
     }
